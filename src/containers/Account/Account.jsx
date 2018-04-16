03fb5fb90593d9fc5b5501351a88c994bfa7cd53
mchri18 <mchri18@lsu.edu>
2018-04-16 12:16:29 -0500

    changed to links to pages

---
+++ src/containers/Account/Account.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Account.css';
+
+const Account = () => (
+  <body>
+    <div class='links'>
+      <a href="/customer">Customer</a>
+    </div>
+    <div class='links'>
+      <a href="/technician">Tech</a>
+    </div>
+    <div class='links'>
+      <a href="/manager">Manager</a>
+    </div>
+  </body>
+);
+
+export default Account;
