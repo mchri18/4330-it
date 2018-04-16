334caece97ba7b17a3acb56c2fc0f8750951153c
mchri18 <mchri18@lsu.edu>
2018-04-16 12:10:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# MODEL
+
+MODEL Computing services is a IT support company that allows its users to request their services online
+
+## Getting Started
+
+### Dependencies
+
+1. Nodejs
+2. Postgresql
+
+### Installation
+
+1. Run the following commands to get started:
+
+* ```sh
+  $ git clone https://github.com/mchri18/4330.git
+  $ cd 4330
+  ```
+
+2. Start the frontend:
+
+* ```sh
+  # Using yarn
+  $ yarn && yarn start
+  ```
+* ```sh
+  # Using npm
+  $ npm install && npm start
+  ```
+
+3. Open a seprate terminal and start the server:
+
+* ```sh
+  # Using yarn
+  $ yarn run server
+  ```
+* ```sh
+  # Using npm
+  $ npm run server
+  ```
---
+++ server/index.js
@@ -0,0 +1,122 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import { createServer } from 'http';
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+
+import { refreshTokens } from './auth';
+import models from './models';
+
+const SECRET = 'ljkblsjdfuwefiuwfwue823yr23';
+const SECRET2 = 'ljkblsjdfuwefiuwfwue823yr23absljasdaudhqud7q8d';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
+
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, './resolvers'))
+);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+
+app.use(cors('*'));
+
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(
+        token,
+        refreshToken,
+        models,
+        SECRET,
+        SECRET2
+      );
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
+const graphqlEndpoint = '/graphql';
+
+app.use(
+  graphqlEndpoint,
+  bodyParser.json(),
+  graphqlExpress(req => ({
+    schema,
+    context: {
+      models,
+      user: req.user,
+      SECRET,
+      SECRET2
+    }
+  }))
+);
+
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: graphqlEndpoint,
+    subscriptionsEndpoint: 'ws://localhost:8081/subscriptions'
+  })
+);
+
+const server = createServer(app);
+
+models.sequelize.sync().then(() => {
+  server.listen(8081, () => {
+    // eslint-disable-next-line no-new
+    new SubscriptionServer(
+      {
+        execute,
+        subscribe,
+        schema,
+        onConnect: async ({ token, refreshToken }, webSocket) => {
+          if (token && refreshToken) {
+            try {
+              const { user } = jwt.verify(token, SECRET);
+              return { models, user };
+            } catch (err) {
+              const newTokens = await refreshTokens(
+                token,
+                refreshToken,
+                models,
+                SECRET,
+                SECRET2
+              );
+              return { models, user: newTokens.user };
+            }
+          }
+
+          return { models };
+        }
+      },
+      {
+        server,
+        path: '/subscriptions'
+      }
+    );
+  });
+});
