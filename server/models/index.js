60298ff9383c8ab3e96600a7e7401c3f0eebf82f
mchri18 <mchri18@lsu.edu>
2018-04-16 12:11:28 -0500

    setting up server

---
+++ server/models/index.js
@@ -0,0 +1,29 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('workflow', 'postgres', 'postgres', {
+  dialect: 'postgres',
+  operatorsAliases: Sequelize.Op,
+  define: {
+    underscored: true
+  }
+});
+
+const models = {
+  User: sequelize.import('./user'),
+  Channel: sequelize.import('./channel'),
+  Message: sequelize.import('./message'),
+  Team: sequelize.import('./team'),
+  Member: sequelize.import('./member'),
+  DirectMessage: sequelize.import('./directMessage')
+};
+
+Object.keys(models).forEach(modelName => {
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
---
+++ server/models/user.js
@@ -0,0 +1,71 @@
+import bcrypt from 'bcrypt';
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: 'Username can only contain letters and numbers.'
+          },
+          len: {
+            args: [7, 25],
+            msg: 'Username must be between 7 and 25 characters long.'
+          }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Not a valid email address.'
+          }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [8, 25],
+            msg: 'Password must be between 8 and 25 characters long.'
+          }
+        }
+      },
+      // manager: DataTypes.STRING,
+      // subscriptionType: DataTypes.STRING
+    },
+    {
+      hooks: {
+        beforeCreate: async user => {
+          const hashedPassword = await bcrypt.hash(user.password, 12);
+          user.password = hashedPassword;
+        }
+      }
+    }
+  );
+  User.associate = models => {
+    User.belongsToMany(models.Team, {
+      through: models.Member,
+      foreignKey: {
+        name: 'userId',
+        field: 'user_id'
+      }
+    });
+    // N:M
+    User.belongsToMany(models.Channel, {
+      through: 'channel_member',
+      foreignKey: {
+        name: 'userId',
+        field: 'user_id'
+      }
+    });
+  };
+
+  return User;
+};
