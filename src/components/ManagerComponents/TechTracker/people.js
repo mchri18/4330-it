8d577d7668fef193abffc17650d83cb77e7676d5
MattCab <mattcabrera2014@yahoo.com>
2018-04-16 19:03:38 -0500

    Add files via upload

---
+++ src/components/ManagerComponents/TechTracker/people.js
@@ -0,0 +1,39 @@
+const people = [{
+    background: '#2980B9',
+    imgSrc: 'http://images5.fanpop.com/image/photos/31300000/Tim-The-Tool-Man-Taylor-tim-allen-31354266-470-476.jpg',
+    imgBorderColor: '#015389',
+    name: 'Tim A.',
+    title: 'Lead Technician',
+    mobileNo: '(123)-456-7891',
+    location: '123 Road drive, Baton Rouge, Louisiana',
+    role: "I carry out jobs and services just like any other technician. I have the most experience out of my staff and I work right under the manager. If you would like to request me specifically leave a note in the service request."
+}, {
+    background: '#27AE60',
+    imgSrc: 'http://resources3.atgtickets.com/static/699_full.jpg',
+    imgBorderColor: '#086C32',
+    name: 'Bob T.B.',
+    title: 'Technician',
+    mobileNo: '(123)-333-4444',
+    location: '456 Street Ave, New Orleans, Louisiana',
+    role: "I am the technician for the New Orleans area. I am a mid-level technician with many years of experience under my belt. I am very handy and can tackle almost anything you throw at me. If you would like to request me specifically leave a note in the service request."
+}, {
+    background: '#9B27AE',
+    imgSrc: 'http://4.bp.blogspot.com/_WrjkqS_HBb8/SN5tlt7923I/AAAAAAAAAWk/zJpFQ2Jso4I/s400/vlcsnap-67213.jpg',
+    imgBorderColor: '#6A067A',
+    name: 'Ryan H.',
+    title: 'Technician',
+    mobileNo: '0491 570 158',
+    location: '993 Lane Blvd, Baton Rouge, Louisiana',
+    role: 'I am a relatively new technician. I have managerial and executive experience so this jobs should be pretty easy. Anyway tweet me if you need some servicing or whatever.If you would like to request me specifically leave a note in the service request.',
+}, {
+    background: '#e67e22',
+    imgSrc: 'https://78.media.tumblr.com/f1dacdc4f6ddfe4de9daa2531ecb0b7c/tumblr_inline_oep69uzjb91sss5ih_400.png',
+    imgBorderColor: '#9D4F09',
+    name: 'Rick S.',
+    title: 'Technician',
+    mobileNo: '0491 570 110',
+    location: '???Earth???',
+    role: "Turns out making money is a requirement so I'm doing this technician thing until I can start reproducing currency in the garage. If you would like to request me specifically: Don't. I won't come. I... I'm serious don't do it."
+}];
+
+export default people;
